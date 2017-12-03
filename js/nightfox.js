79aff76092d007f88cb17a27c1668cd534b85e36
Kelly <mitiao888kelly@gmail.com>
2017-12-03 23:10:31 +0000

    correct and incorrect is working

--- js/nightfox.js
+++ js/nightfox.js
@@ -90,6 +90,7 @@ function clickmovie(select)
 
     else if(cur_section =="movie")
     {
+        cur_section = "play_movie";
         var play = document.getElementById("myContainer");
         if(select==1)
             play.innerHTML='<iframe width="'+width+'" height="'+height+'" src="https://www.youtube.com/embed/D6oj6zBHu8o?autoplay=1" frameborder="0" allowfullscreen></iframe>';
@@ -150,6 +151,8 @@ function clickac()
         ac.innerHTML="<h1>current temprature</h1>";
     }
 }
+
+//Hide and show items
 function train()
 {
     var hideItem=["movieAnimation","foodAnimation","bookAnimation","acAnimation"];
@@ -157,6 +160,14 @@ function train()
 }
 function home()
 {
+    var h1 = document.getElementById("movieAnimation");
+    var h2 = document.getElementById("foodAnimation");
+    var h3 = document.getElementById("bookAnimation");
+    var h4 = document.getElementById("acAnimation");
+    h1.style.backgroundImage = "url('http://icons.iconarchive.com/icons/designbolts/free-multimedia/1024/Clapper-icon.png')";
+    h2.style.backgroundImage = "url('http://www-static.dreambox.com/wp-content/uploads/2015/02/Prize-Icon-Pizza.png')";
+    h3.style.backgroundImage = "url('https://freeiconshop.com/wp-content/uploads/edd/book-flat.png')";
+    h4.style.backgroundImage = "url('https://daks2k3a4ib2z.cloudfront.net/58adc68142ff652745bbf599/58add3fc4effab671dfe7da6_Ikes%20Icon-03.png')";
     var hideItem=["movieAnimation","foodAnimation","bookAnimation","acAnimation"];
     Element(hideItem,'s');
 }
@@ -182,3 +193,68 @@ function Element(hideItem, operation)
     }
     
 }
+
+function correct()
+{
+    var select = 1;//select=templatematching();
+    if(cur_section == "home")
+    {
+        if(select == 1)
+            clickmovie();
+        else if(select == 2)
+            clickfood();
+        else if(select == 3)
+            clickbook();
+        else
+            clickac();
+    }
+    else if(cur_section == "movie")
+    {
+        clickmovie(select);
+    }
+    else if(cur_section =="food")
+    {
+        clickfood(select);
+    }
+    else if(cur_section == "book")
+    {
+        clickbook(select);
+    }
+    else if(cur_section == "ac")
+    {
+        clickac(select);
+    }
+}
+
+function incorrect()
+{
+    if(cur_section =="play_movie"||cur_section == "ac" )
+    {
+        
+        var h=document.getElementById("myContainer");
+        h.innerHTML = '<div id ="movieAnimation"></div><div id ="foodAnimation"></div><div id ="bookAnimation"></div><div id ="acAnimation"></div>';
+        home();
+        if(cur_section == "play_movie")
+        {
+            cur_section ="home";
+            clickmovie();
+        }
+        else if(cur_section == "ac")
+        {
+            cur_section ="home";
+            clickac();
+        }
+    }
+    else if(cur_section == "movie" || cur_section == "food" || cur_section == "book" || cur_section == "ac")
+    {
+        cur_section="home";
+        home();
+    }
+    else
+    {
+        cur_section = "home";
+        home();
+    }
+}
+
+
